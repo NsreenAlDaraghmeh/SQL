aaffc38da69fa1c6d0f7c28dfa20155f4cfbc295
NsreenAlDaraghmeh <115452541+NsreenAlDaraghmeh@users.noreply.github.com>
2022-12-12 13:27:34 +0000

    Add files via upload

---
+++ SQLQuery1.sql
@@ -0,0 +1,30 @@
+create table customers(
+id int primary key ,
+firstname varchar(20),
+lastname varchar(20),
+);
+select*from customers ;
+
+
+create table orders(
+id int primary key ,
+price float,
+total float,
+);
+select*from orders ;
+
+
+create table products(
+id int primary key ,
+name varchar(20),
+price float ,
+);
+select*from products ;
+
+
+create table Employees(
+id int primary key ,
+firstname varchar(20),
+lastname varchar(20),
+);
+select*from Employees ;
